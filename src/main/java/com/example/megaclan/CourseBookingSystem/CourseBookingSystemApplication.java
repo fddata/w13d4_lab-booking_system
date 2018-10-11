5e550826282af90f4fb81c4169e2740c5d465b1d
fddata <fraserjdouglas@gmail.com>
2018-10-11 10:53:55 +0100

    inital commit

---
+++ src/main/java/com/example/megaclan/CourseBookingSystem/CourseBookingSystemApplication.java
@@ -0,0 +1,12 @@
+package com.example.megaclan.CourseBookingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseBookingSystemApplication.class, args);
+	}
+}
