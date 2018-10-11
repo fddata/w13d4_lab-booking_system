bda94a379f78f3d6b84fdd537d85d76a74b9e494
fddata <fraserjdouglas@gmail.com>
2018-10-11 11:02:43 +0100

    adds classes to models

---
+++ src/main/java/com/example/megaclan/CourseBookingSystem/models/Booking.java
@@ -0,0 +1,54 @@
+package com.example.megaclan.CourseBookingSystem.models;
+
+public class Booking {
+
+    private Long id;
+
+
+    private String date;
+
+    private Customer customer;
+
+    private Course course;
+
+    public Booking(String date, Customer customer, Course course) {
+        this.date = date;
+        this.customer = customer;
+        this.course = course;
+    }
+
+    public Booking() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+}
